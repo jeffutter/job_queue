ad390c5c780fb7a982e64edf9ca7a49c90bb5dd4
Jeffery Utter <jeff@jeffutter.com>
2017-11-20 21:51:00 -0600

    Add Inch CI

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule JobQueue.Mixfile do
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:inch_ex, only: :docs},
       gen_stage: "~> 0.12.1"
     ]
   end
